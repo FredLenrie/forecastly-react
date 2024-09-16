8c87e6019425714272f70ca050a4f3fdbe06b236
FredLenrie <fredlenrieb17@gmail.com>
2024-09-16 14:21:41 +0000

    change of home section design

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,44 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './HomeSection.css';
 
 const HomeSection = () => {
-  const images = [
-    `${process.env.PUBLIC_URL}/image1.jpg`,
-    `${process.env.PUBLIC_URL}/image2.jpg`,
-    `${process.env.PUBLIC_URL}/image3.jpg`,
-    `${process.env.PUBLIC_URL}/image4.jpg`,
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Change image every 5 seconds
-
-    // Cleanup the interval on component unmount
-    return () => clearInterval(intervalId);
-  }, [images.length]);
-
   return (
     <section id="home" className="home-section">
-      <section className="image-slider">
-      <img
-        id="slider-img"
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex + 1}`}
-      />
-      </section>
+        {/* Text Section */}
+        <div className="text-section">
+          <h1>Hi! WELCOME TO<span> FORECASTLY</span></h1>
+          <p>
+          Get information about the weather and our services. Stay tuned for updates and weather forecasts tailored 
+          to your needs. Enjoy the weather with Forecastly! Your trusted source for real-time weather updates.
+          </p>
 
-      <section className="text-content">
-        <h1>WELCOME TO FORECASTLY</h1>
-      </section>
+          <div className="home-buttons">
+            <a href="#about">
+              <button type="button" className="button">
+                <span className="button-span"></span>LEARN MORE
+              </button>
+            </a>
+            <a href="#how-it-works">
+              <button type="button" className="button">
+                <span className="button-span"></span>CHECK WEATHER
+              </button>
+            </a>
+          </div>
+        </div>
 
-      <div className="home-content">
-        <p>
-          Get information about the weather and our services. Stay tuned for updates and weather forecasts tailored to your needs. Enjoy the weather with Forecastly! Your trusted source for real-time weather updates.
-        </p>
-      </div>
+        {/* Image Section */}
+        <div className="image-section">
+          <video autoPlay muted loop className="background-video">
+            <source src={`${process.env.PUBLIC_URL}/background1.mp4`} type="video/mp4" />
+          </video>
+          <img src={`${process.env.PUBLIC_URL}/image.jpg`} className="img-2" alt="Overlapping Image" />
+        </div>
     </section>
   );
 };
